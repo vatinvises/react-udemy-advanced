fb7adc47437e2bc983447fea64c07e28249fc252
Tanyalak <t.vatinvises@gmail.com>
2020-10-24 21:23:16 +0200

    useState Array  clear all from the list  delete one by one (using filter)
    Signed-off-by: Tanyalak <t.vatinvises@gmail.com>

--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
+//array of objects
 export const data = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'peter' },
-  { id: 3, name: 'susan' },
-  { id: 4, name: 'anna' },
+  { id: 1, name: "john" },
+  { id: 2, name: "peter" },
+  { id: 3, name: "susan" },
+  { id: 4, name: "anna" },
 ];
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data); // <- for use it just once OR either import React, { useState } from "react";
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id != id); // filter unwanted it
+    setPeople(newPeople);
+  };
+  return (
+    // list, need key prop
+    // clear all button
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => removeItem(id)}>
+              delete me
+            </button>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear item
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
