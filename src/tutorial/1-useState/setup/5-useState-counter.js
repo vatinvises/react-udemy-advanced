6612ada52f4335eb66726f62a58016883c0b7888
Tanyalak <t.vatinvises@gmail.com>
2020-10-24 22:07:21 +0200

    2 options of updating form  1 value update form***  2 functional update form

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+//2 options of updating form
+//1 value update form***
+//2 functional update form
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>useState counter example</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          decrease
+        </button>
+        <button className="btn" onClick={() => setValue(0)}>
+          reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          increase
+        </button>
+      </section>
+    </>
+  );
 };
-
 export default UseStateCounter;
