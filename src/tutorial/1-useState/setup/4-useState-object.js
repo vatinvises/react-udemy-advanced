935b4d4296269d61d267cac61667f109fcd5f1e9
Tanyalak <t.vatinvises@gmail.com>
2020-10-24 21:59:20 +0200

    useState Object  set up 3 seperate state values, and use handler method  dont forget to use spread to preserve the other states
    Signed-off-by: Tanyalak <t.vatinvises@gmail.com>

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -8,14 +8,21 @@ const UseStateObject = () => {
     age: 24,
     message: "random message",
   });
+
+  //set up 3 seperate state values, and use handler method
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState("24");
+  const [message, setMessage] = useState("random message");
   const changeMessage = () => {
-    setPerson({ ...person, message: "hello world" }); //set up as msg and pass as prop of mes, use SPREAD operator to prevent a wipeout (leave things as they are and overwrite just message)
+    //  setPerson({ ...person, message: "hello world" }); //set up as msg and pass as prop of mes, use SPREAD operator to prevent a wipeout (leave things as they are and overwrite just message)
+    setMessage("hello world");
   };
+  //delete person because we re not dealing with obj
   return (
     <>
-      <h2>{person.name}</h2>
-      <h2>{person.age}</h2>
-      <h2>{person.message}</h2>
+      <h2>{name}</h2>
+      <h2>{age}</h2>
+      <h2>{message}</h2>
       <button className="btn" onClick={changeMessage}>
         change message
       </button>
