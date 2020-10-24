c1fb93ca44294187da2622840f5cd1d19634ac78
Tanyalak <t.vatinvises@gmail.com>
2020-10-24 21:42:17 +0200

    useState Object  set only 1 prop of object  use spread operator to prevent a wipeout  and overwrite just the required prop Signed-off-by: Tanyalak <t.vatinvises@gmail.com>

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Fragment } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  //person as object
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+  const changeMessage = () => {
+    setPerson({ ...person, message: "hello world" }); //set up as msg and pass as prop of mes, use SPREAD operator to prevent a wipeout (leave things as they are and overwrite just message)
+  };
+  return (
+    <>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.message}</h2>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
