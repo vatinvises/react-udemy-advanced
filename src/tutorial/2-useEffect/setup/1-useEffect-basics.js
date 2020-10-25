a9c6c8415dec6353589a5fc0df9e9f8ffd2f2e03
Tanyalak <t.vatinvises@gmail.com>
2020-10-25 18:13:51 +0100

    useEffect basic  change tab message

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
+// useEffect = work outside the component
+// pass in callback function
+// when render component -> useEffect is called
+// render twice because of strict mode
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0); //preserve the value between renders
+  useEffect(() => {
+    document.title = `new message(${value})`; //tab with number(x)
+  }); // runs after every render
+  return (
+    <>
+      <h2>{value}</h2>
+      <button
+        className="btn"
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        increase
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
