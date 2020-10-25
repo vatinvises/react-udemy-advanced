2b7afdcb005d55af9f9fcf27967175d21d750381
Tanyalak <t.vatinvises@gmail.com>
2020-10-25 19:38:41 +0100

    useEffect fetch data, get and set triggers rerendering->prevent infinite loop by using infinite array []
    Signed-off-by: Tanyalak <t.vatinvises@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
-import Final from "./tutorial/2-useEffect/final/2-useEffect-cleanup";
+import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import Final from "./tutorial/2-useEffect/final/3-useEffect-fetch-data";
 function App() {
   return (
     <div className="container">
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users); //trigger rerender in getUser ->infiniteloop!!!!!
+  };
+  useEffect(() => {
+    //async await return a promise, cant be used
+    getUsers();
+  }, []); //prevent infinite loop!!!!!
+
+  //ul = unordered list
+  //when there is a list-> use unique key --> key={id}
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_hrl } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_hrl}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
