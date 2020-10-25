5b5df115c4cac7b930c6238432459fbe1bce6d50
Tanyalak <t.vatinvises@gmail.com>
2020-10-25 18:45:08 +0100

    useEffect run once at the beginning or run everytime the dependency value changes
    Signed-off-by: Tanyalak <t.vatinvises@gmail.com>

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
-// second parameter
+// second parameter =  array(list) of dependency
 
 // useEffect = work outside the component
 // pass in callback function
@@ -10,12 +10,14 @@ import React, { useState, useEffect } from "react";
 
 // condition has to be used inside useEffect callback function
 const UseEffectBasics = () => {
+  // runs after every render
   const [value, setValue] = useState(0); //preserve the value between renders
   useEffect(() => {
     if (value >= 1) {
       document.title = `new message(${value})`; //tab with number(x)
     }
-  }); // runs after every render
+    //  }, []); // array(list) of dependency, blank [] = run only on the initial run
+  }, [value]); // also run once we increase (dependency)value
   return (
     <>
       <h2>{value}</h2>
