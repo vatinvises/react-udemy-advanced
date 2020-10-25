5caa6fd3c57c0879010415e617c2504937ba267a
Tanyalak <t.vatinvises@gmail.com>
2020-10-25 19:02:19 +0100

    useEffect (window) eventListener, set up, clean up and remove
    Signed-off-by: Tanyalak <t.vatinvises@gmail.com>

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import { cleanup } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
+// windows size
+// eventListener
+// prob -> many eventListeners
+// must be clean up
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [value, setValue] = useState(window.innerWidth);
+  const setSize = () => {
+    //triggering rerender because size change, state preserves the value
+    setValue(window.innerWidth);
+  };
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      // invoke when exit
+      window.removeEventListener("resize", checkSize); // the same callback function has to be written
+    };
+  }); //[] wont workt with conditional render
+  return (
+    <>
+      <h2>useEffect cleanup</h2>
+      <h3>windows size </h3>
+      <h1> {value} Px</h1>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
