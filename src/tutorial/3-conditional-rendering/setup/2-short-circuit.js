6a8c5bed35ae76cf19b6a359750b7f9c60d6b088
Tanyalak <t.vatinvises@gmail.com>
2020-11-15 20:52:53 +0100

    state toggling

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,19 +4,23 @@ import React, { useState } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("");
+  const [isError, setError] = useState(false);
   const firstValue = text || "hello world"; // AND  " " -> 0 || "h" = "h"
   const secondValue = text && "hello world"; // OR  "k" -> 1 && "h" = "h"
   // OR if text is empty, return john doe
   // AND if it is true, return the element
   // AND if it is false, return the element
 
+  //how to toggle
   return (
     <>
       {/*    <h1>{firstValue}</h1>
       <h1>value: {secondValue}</h1> */}
       <h1>{text || "display if there is no text"}</h1>
-      {text && <h1>text is {text}</h1>}
-      {!text && <h1>text is empty</h1>}
+      <button className="btn" onClick={() => setError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error..</h1>}
     </>
   );
 };
