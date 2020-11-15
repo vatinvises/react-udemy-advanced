6832f51d40999b7b5daf0fe2e8dd006c57f4881c
Tanyalak <t.vatinvises@gmail.com>
2020-11-15 21:43:29 +0100

    toggle component, use eventlistener and dont forget to remove it!

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      {" "}
+      <button className="btn" onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show ? <Item /> : <h1></h1>}
+    </>
+  );
 };
 
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  //add the following lines for autochange/ doesnt need onClick
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize); // add <--> remove
+    };
+  }, []); // empty dependency
+  return (
+    <div style={{ marginTop: "2erem" }}>
+      <h1>window</h1>
+      <h2>size: {size} pixel</h2>
+    </div>
+  );
+};
 export default ShowHide;
