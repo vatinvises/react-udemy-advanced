bfbc436d4c92a63ce3e79b36596f5a7125e4b1a2
Tanyalak <t.vatinvises@gmail.com>
2020-11-15 20:32:08 +0100

    Short circuit: more on true false condition
    Signed-off-by: Tanyalak <t.vatinvises@gmail.com>

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -6,11 +6,17 @@ const ShortCircuit = () => {
   const [text, setText] = useState("");
   const firstValue = text || "hello world"; // AND  " " -> 0 || "h" = "h"
   const secondValue = text && "hello world"; // OR  "k" -> 1 && "h" = "h"
+  // OR if text is empty, return john doe
+  // AND if it is true, return the element
+  // AND if it is false, return the element
 
   return (
     <>
-      <h1>{firstValue}</h1>
-      <h1>value: {secondValue}</h1>
+      {/*    <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      <h1>{text || "display if there is no text"}</h1>
+      {text && <h1>text is {text}</h1>}
+      {!text && <h1>text is empty</h1>}
     </>
   );
 };
